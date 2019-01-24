35e66d722b0544041ab3fe044d325e21d41f562c
BIT <BIT@DESKTOP-E23PFRN>
2019-01-24 10:52:02 +0900

    190124 10:51 Complete

--- src/com/douzon/guestbook/servlet/GuestbookServlet.java
+++ src/com/douzon/guestbook/servlet/GuestbookServlet.java
@@ -19,13 +19,43 @@ public class GuestbookServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+
 		//요청 분리(요청식별)
 		String action = request.getParameter("a");
 		
-		if("index".equals(action)) {
-			
-		} else if("deleteform".equals(action)) {
+		if("deleteform".equals(action)) {
 			WebUtils.forward(request, response, "/WEB-INF/views/deleteform.jsp");
+		} else if("delete".equals(action)) {
+			
+			String no_string = request.getParameter("no");
+			Long no = Long.parseLong(no_string);
+			String password = request.getParameter("password");
+			
+			GuestbookVo vo = new GuestbookVo();
+			
+			vo.setNo(no);
+			vo.setPassword(password);
+			
+			new GuestbookDao().delete(vo);
+			
+			WebUtils.redirect(request, response, request.getContextPath()+"/gb");
+			
+		} else if("add".equals(action)) {
+
+			String name = request.getParameter("name");
+			String password = request.getParameter("password");
+			String message = request.getParameter("message");
+			
+			GuestbookVo vo = new GuestbookVo();
+			vo.setName(name);
+			vo.setPassword(password);
+			vo.setMessage(message);
+			
+			new GuestbookDao().insert(vo);
+			
+			WebUtils.redirect(request, response, request.getContextPath()+"/gb");
+			
 		} else {
 			/* default action : 디폴트 요청 처리 */
 			/* list를 보여주면 좋다 = index.jsp */
